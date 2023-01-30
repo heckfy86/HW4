4e53a889198d4a86528881e977cd51d591c74be1
heckfy86 <gmail08@yandex.ru>
2023-01-30 14:59:54 +0100

    HW10_9

--- src/test/java/democa/TestBaseExtended.java
+++ src/test/java/democa/TestBaseExtended.java
@@ -32,6 +32,8 @@ public class TestBaseExtended {
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
